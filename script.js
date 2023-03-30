6e01f8671f692c4ba9c098062d5ba54e57f77de1
feduarte-dev <127968783+feduarte-dev@users.noreply.github.com>
2023-03-30 16:09:04 -0300

    first model complete, waiting for user approval

---
+++ script.js
@@ -0,0 +1,91 @@
+// Funções hide-show das especialidades
+function myFunction1() {
+  var x = document.getElementById("texto1")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("texto2")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("texto3")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function myFunction4() {
+  var x = document.getElementById("texto4")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function myFunction5() {
+  var x = document.getElementById("texto5")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function myFunction6() {
+  var x = document.getElementById("texto6")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function myFunction7() {
+  var x = document.getElementById("texto7")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function myFunction8() {
+  var x = document.getElementById("texto8")
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block"
+  }
+}
+
+// Função de movimento das especialidades
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal")
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight
+    var elementTop = reveals[i].getBoundingClientRect().top
+    var elementVisible = 150
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active")
+    } else {
+      reveals[i].classList.remove("active")
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal)
