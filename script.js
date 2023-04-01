7bf81626f08482ce50889fdde93f5ce29439a643
feduarte-dev <127968783+feduarte-dev@users.noreply.github.com>
2023-04-01 17:15:18 -0300

    updated texts, logo and fonts

--- script.js
+++ script.js
@@ -1,91 +1,91 @@
 // Funções hide-show das especialidades
 function myFunction1() {
-  var x = document.getElementById("texto1")
+  var x = document.getElementById("texto1");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 function myFunction2() {
-  var x = document.getElementById("texto2")
+  var x = document.getElementById("texto2");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 function myFunction3() {
-  var x = document.getElementById("texto3")
+  var x = document.getElementById("texto3");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 function myFunction4() {
-  var x = document.getElementById("texto4")
+  var x = document.getElementById("texto4");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 function myFunction5() {
-  var x = document.getElementById("texto5")
+  var x = document.getElementById("texto5");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 function myFunction6() {
-  var x = document.getElementById("texto6")
+  var x = document.getElementById("texto6");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 function myFunction7() {
-  var x = document.getElementById("texto7")
+  var x = document.getElementById("texto7");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 function myFunction8() {
-  var x = document.getElementById("texto8")
+  var x = document.getElementById("texto8");
   if (x.style.display === "block") {
-    x.style.display = "none"
+    x.style.display = "none";
   } else {
-    x.style.display = "block"
+    x.style.display = "block";
   }
 }
 
 // Função de movimento das especialidades
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal")
+  var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight
-    var elementTop = reveals[i].getBoundingClientRect().top
-    var elementVisible = 150
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active")
+      reveals[i].classList.add("active");
     } else {
-      reveals[i].classList.remove("active")
+      reveals[i].classList.remove("active");
     }
   }
 }
 
-window.addEventListener("scroll", reveal)
+window.addEventListener("scroll", reveal);
